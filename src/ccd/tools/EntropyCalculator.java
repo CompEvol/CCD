71ff1f0dac87ed1c916330007f4914c979536d03
Remco Bouckaert <higgs.ml@gmail.com>
2025-11-26 19:38:08 +1300

    add multi tree processing + trace output options to EntropyCalculator

--- src/ccd/tools/EntropyCalculator.java
+++ src/ccd/tools/EntropyCalculator.java
@@ -1,10 +1,15 @@
 package ccd.tools;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import beast.base.core.Description;
 import beast.base.core.Input;
 import beast.base.core.Log;
 import beastfx.app.tools.Application;
 import beastfx.app.treeannotator.TreeAnnotator.MemoryFriendlyTreeSet;
+import beastfx.app.util.OutFile;
 import beastfx.app.util.TreeFile;
 import ccd.model.AbstractCCD;
 import ccd.model.CCDType;
@@ -12,8 +17,10 @@ import ccd.model.CCDType;
 @Description("Calculates the phylogenetic entropy of the posterior tree distribution")
 public class EntropyCalculator extends beast.base.inference.Runnable {
     // input
-    final public Input<TreeFile> treeInput = new Input<>("trees", "trees file for which to compute entropy", Input.Validate.REQUIRED);
+    final public Input<List<TreeFile>> treeInput = new Input<>("trees", "trees file for which to compute entropy", new ArrayList<>(), Input.Validate.REQUIRED);
     final public Input<Integer> burnInPercentageInput = new Input<>("burnin", "percentage of trees to used as burn-in (and will be ignored)", 10);
+    final public Input<Boolean> verboseInput = new Input<>("verbose", "print info to stdout -- set to false to minimise output", true);
+    final public Input<OutFile> outputInput = new Input<>("output", "name of the output file to store results. If not specified, use stdout");
 
     // configuration
     final public Input<String> ccdTypeInput = new Input<>("ccdType", "CCD0, CCD1, or CCD2", "CCD0");
@@ -25,16 +32,38 @@ public class EntropyCalculator extends beast.base.inference.Runnable {
 
     @Override
     public void run() throws Exception {
-        Log.info("# Entropy Calculator");
-        Log.info("> with the following parameters...");
-        Log.info("    trees file: " + treeInput.get().getPath());
-        Log.info("    burnin:     " + burnInPercentageInput.get());
-        Log.info("    CCD type:   " + CCDType.fromName(ccdTypeInput.get()));
-
-        MemoryFriendlyTreeSet treeSet = CCDToolUtil.getTreeSet(treeInput, burnInPercentageInput.get());
-        AbstractCCD ccd = CCDToolUtil.getCCDTypeByName(treeSet, ccdTypeInput);
-
-        Log.info("\nPhylogenetic Entropy: " + ccd.getEntropy());
+    	
+    	
+    	PrintStream trace = null;
+    	if (outputInput.get() != null) {
+    		trace = new PrintStream(outputInput.get());
+    		trace.println("sample\tfile\tentropy");
+    	}
+    		
+    	int k = 0;
+    	
+    	for (TreeFile treefile : treeInput.get()) {
+    		if (verboseInput.get()) {
+		        Log.info("# Entropy Calculator");
+		        Log.info("> with the following parameters...");
+		        Log.info("    trees file: " + treefile.getPath());
+		        Log.info("    burnin:     " + burnInPercentageInput.get());
+		        Log.info("    CCD type:   " + CCDType.fromName(ccdTypeInput.get()));
+    		}
+	
+	        MemoryFriendlyTreeSet treeSet = CCDToolUtil.getTreeSet(treefile.getPath(), burnInPercentageInput.get());
+	        AbstractCCD ccd = CCDToolUtil.getCCDTypeByName(treeSet, ccdTypeInput);
+	        
+	        double entropy = ccd.getEntropy();
+	
+	        if (verboseInput.get()) {
+	        	Log.info("\nPhylogenetic Entropy: " + entropy);
+	        }
+	        if (trace != null) {
+	        	trace.println(k + "\t" + treefile.getName() + "\t" + entropy);
+	        }
+	        k++;
+    	}
     }
 
     public static void main(String[] args) throws Exception {
